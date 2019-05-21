2d8afaaed651dd383819918070090eaaf44fdc84
GunjiD <GunjiD>
2019-05-21 22:07:46 +0900

    スレッドプログラムのサンプルを追加

--- README.md
+++ README.md
@@ -1,3 +1,7 @@
 # automatic-baking
 
 クッキーを自動で焼きます  
+
+## thred-sample.py についてのメモ
+
+thred は start() された時点で前の thred を破棄するため食いつぶすことはない  
--- thred-sample.py
+++ thred-sample.py
@@ -1,8 +1,9 @@
+import time
+import threading
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
-import time
 
 def save_local():
     file = open('savedata.txt', 'w')
@@ -17,15 +18,21 @@ def load_local():
     file.close()
     return
 
+def click_cookie():
+    while True:
+        driver.execute_script('Game.ClickCookie()')
+
 driver = webdriver.Chrome("/usr/local/bin/chromedriver")
-driver.get('https://orteil.dashnet.org/cookieclicker/')
+driver.get('http://natto0wtr.web.fc2.com/CookieClicker/')
 time.sleep(5)
 load_local()
 time.sleep(5)
 
-while True:
-    for i in range(1000):
-        driver.execute_script('Game.ClickCookie()')
-    save_local()  #ローカルにセーブデータを保存
+if __name__ == "__main__":
+    thread_click = threading.Thread(target=click_cookie)
+    thread_save_local = threading.Timer(10,save_local)
 
-driver.quit()
+    thread_click.start()
+    thread_save_local.start()
+
+#driver.quit()
